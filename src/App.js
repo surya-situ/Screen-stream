70bc8d9a326a3ac0cdc6e02faaf5ef152e09f10d
surya-situ <suryasocial55@gmail.com>
2023-12-27 21:29:33 +0530

    Add store provider to the app

--- src/App.js
+++ src/App.js
@@ -1,24 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { Outlet } from "react-router-dom";
+import { Provider } from "react-redux";
 
-function App() {
+import appStore from './utils/store'
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+
+const App = () => {
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Provider store={appStore}>
+      <div className="bg-gradient-to-b from-blue-950 via-zinc-950  to-black text-white">
+        <Header />
+        <div className="pl-[130px]">
+          <Outlet />
+          <Footer />
+        </div>
+      </div>
+    </Provider>
   );
 }
 
