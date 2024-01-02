763c0cc940e666ea9518d52dbda8d27016bc0007
surya-situ <suryasocial55@gmail.com>
2024-01-02 20:26:32 +0530

    revert: changed it to ES5 config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,8 @@
 /** @type {import('tailwindcss').Config} */
-export const content = ["./src/**/*.{html,js}"];
-export const theme = {
-  extend: {},
-};
-export const plugins = [];
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
