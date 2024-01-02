c3c315c641941d247008018161933058482d3e21
surya-situ <suryasocial55@gmail.com>
2024-01-02 20:15:23 +0530

    feat: Changed the background color of the on hover and made all the links zoom in-out on hover

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,5 +1,5 @@
+import React, {useEffect} from 'react'
 import { Link } from 'react-router-dom'
-
 import AccountCircleOutlinedIcon from '@mui/icons-material/AccountCircleOutlined';
 import HomeOutlinedIcon from '@mui/icons-material/HomeOutlined';
 import SearchOutlinedIcon from '@mui/icons-material/SearchOutlined';
@@ -8,68 +8,90 @@ import LocalMoviesOutlinedIcon from '@mui/icons-material/LocalMoviesOutlined';
 import CategoryOutlinedIcon from '@mui/icons-material/CategoryOutlined';
 
 import logo from '../images/logo.png'
-import '../styles/Header.css'
+import './Header.css'
 
 const Header = () => {
 
+    useEffect(() => {
+        const sidebar = document.getElementById('sidebar');
+        const handleScroll = () => {
+          const scrollY = window.scrollY;
+
+          if (scrollY > sidebar.offsetTop) {
+            sidebar.style.position = 'fixed';
+            sidebar.style.left = '0';
+            sidebar.style.top = '0';
+          } else {
+            sidebar.style.position = 'absolute';
+            sidebar.style.left = '0';
+            sidebar.style.top = `${sidebar.offsetTop}px`;
+          }
+        };
+
+        window.addEventListener('scroll', handleScroll);
+
+        return () => {
+          window.removeEventListener('scroll', handleScroll);
+        };
+    }, []);
+
   return (
-    <div  className=''>
-        
-        <div>
+    <div id="sidebar" className='h-screen flex pt-10 w-[120px] text-white px-2 relative'>
+        <div className='sidebar-background'></div>
+        <ul className='flex flex-col items-center space-y-12 cursor-pointer'>
             <li>
                 <Link to="/subscriptionPage">
-                    <img className='' src={logo} alt='logo'/>
+                    <div className='relative'>
+                        <img src={logo} alt='logo'/>
+                    </div>
                 </Link>
             </li>
-        </div>
-
-        <ul className=''>
-            <li>
+            <li className='glow'>
                 <Link to="/userPage">
-                    <div className=''>
+                    <div className='relative'>
                         <AccountCircleOutlinedIcon />
-                        <span className=''>Account</span>
+                        <span className='tooltip'>Account</span>
                     </div>
                 </Link>
             </li>
-            <li>
+            <li className='glow'>
                 <Link to="/" title='Home'>
-                    <div className=''>
+                    <div className='relative'>
                         <HomeOutlinedIcon />
-                        <span className=''>Home</span>
+                        <span className='tooltip'>Home</span>
                     </div>
                 </Link>
             </li>
-            <li>
+            <li className='glow'>
                 <Link to="/searchPage">
-                    <div className=''>
+                    <div className='relative'>
                         <SearchOutlinedIcon />
-                        <span className=''>Search</span>
+                        <span className='tooltip'>Search</span>
                     </div>
                 </Link>
             </li>
-            <li>
+            <li className='glow'>
                 <Link to="/tvPage">
-                    <div className=''>
+                    <div className='relative'>
                         <TvOutlinedIcon />
-                        <span className=''>TV</span>
+                        <span className='tooltip'>TV</span>
                     </div>
                 </Link>
             </li>
-            <li>
+            <li className='glow'>
                 <Link to="/moviePage">
-                    <div className=''>
-                        <LocalMoviesOutlinedIcon  className=''/>
-                        <span className=''>Movies</span>
+                    <div className='relative'>
+                        <LocalMoviesOutlinedIcon />
+                        <span className='tooltip'>Movies</span>
                     </div>
                 </Link>
             </li>
-            <li>
+            <li className='glow'>
                 <Link to="/categoryPage">
                     {/* <CategoryOutlinedIcon /> */}
-                    <div className="">
+                    <div className="relative">
                         <CategoryOutlinedIcon />
-                        <span className="">Categories</span>
+                        <span className="tooltip">Categories</span>
                     </div>
                 </Link>
             </li>
