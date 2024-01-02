93b51d79ce47c365c691db16906439fc29d8a27b
surya-situ <suryasocial55@gmail.com>
2024-01-02 20:20:44 +0530

    fix: Changed the background color

--- src/App.js
+++ src/App.js
@@ -6,16 +6,15 @@ import appStore from './utils/store'
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 
+
 const App = () => {
 
   return (
     <Provider store={appStore}>
-      <div className="bg-gradient-to-b from-blue-950 via-zinc-950  to-black text-white">
+      <div className="p-0 m-0 text-white bg-gradient-to-b from-indigo-950 via-slate-900 to-black">
         <Header />
-        <div className="pl-[130px]">
-          <Outlet />
-          <Footer />
-        </div>
+        <Outlet />
+        <Footer />
       </div>
     </Provider>
   );
