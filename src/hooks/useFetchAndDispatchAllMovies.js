440b95b0b9427143df64933f1a5c5575e8db4e6c
surya-situ <suryasocial55@gmail.com>
2024-01-02 20:25:25 +0530

    feat: Custom hook for all movie list

---
+++ src/hooks/useFetchAndDispatchAllMovies.js
@@ -0,0 +1,24 @@
+import { useEffect } from "react"
+import { useDispatch } from "react-redux"
+
+import { SCREEN_STREAM_API } from "../utils/constants";
+import { addAllMovies } from "../utils/movieSlice";
+
+
+const useFetchAndDispatchAllMovies = () => {
+
+    const dispatch = useDispatch()
+
+    const getAllMovies = async() => {
+        const data = await fetch('https://api.themoviedb.org/3/movie/now_playing?language=en-US&page=1', SCREEN_STREAM_API)
+        const json = await data.json()
+        dispatch(addAllMovies(json.results))
+    }
+
+    useEffect(() => {
+        getAllMovies()
+    },[])
+
+}
+
+export default useFetchAndDispatchAllMovies;
