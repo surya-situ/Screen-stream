89fd1340c9176322caa23b04569294be2908efbf
surya-situ <suryasocial55@gmail.com>
2024-01-03 16:51:22 +0530

    style: set the video height to full to make fit as per video aspect

--- src/components/Trailer.js
+++ src/components/Trailer.js
@@ -8,9 +8,9 @@ const Trailer = ({movieID}) => {
   useFetchAndDispatchMovieTrailer({movieID});
 
   return (
-    <div className=''>
+    <div>
       <iframe 
-        className='w-screen h-auto aspect-video'
+        className='w-screen h-fit aspect-video'
         src={`https://www.youtube.com/embed/${trailer?.key}?version=3&autoplay=1&controls=0&showinfo=0&loop=1&mute=1&autohide=2&disablekb=1&rel=0&modestbranding=1&iv_load_policy=3`}
         title="YouTube video player" 
         allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" 
