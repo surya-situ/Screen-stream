02dbc44235ed7f3af66135b49bbf581ece2e3d81
surya-situ <suryasocial55@gmail.com>
2024-01-04 22:17:13 +0530

    api: api header for screen-stream form TMDB

---
+++ src/utils/constants.js
@@ -0,0 +1,7 @@
+export const SCREEN_STREAM_API = {
+    method: 'GET',
+    headers: {
+      accept: 'application/json',
+      Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIwYWY4ZTlmYzUzMmIwMTk0ZWI3N2E3MTU0MTNiYTgxZCIsInN1YiI6IjY1N2U4M2VmZjFiNTcxMDhiMWFkOWExMSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.RCc5CFuo3d1zw2m7Q3qfihr86EdYOsoP5gqFLoCyLuA'
+    }
+};
