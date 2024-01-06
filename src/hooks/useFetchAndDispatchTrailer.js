308f3b58658a46e7ba5648cdabd57859712e53c6
surya-situ <suryasocial55@gmail.com>
2024-01-06 10:06:56 +0530

    fix: manually returning to trigger fetch

--- src/hooks/useFetchAndDispatchTrailer.js
+++ src/hooks/useFetchAndDispatchTrailer.js
@@ -4,19 +4,6 @@ import { useDispatch } from "react-redux";
 import { SCREEN_STREAM_API } from "../utils/constants";
 import { addMovieTrailer } from "../utils/movieSlice";
 
-// useEffect(() => {
-//   const fetchMovieTrailer = async () => {
-//     const data = await fetch(`https://api.themoviedb.org/3/movie/${movieID}/videos?language=en-US`, SCREEN_STREAM_API)
-//     const json = await data.json();
-
-//     const filterData = json.results.filter((video)=> video.type === "Trailer")
-//     const trailer = filterData.length ? filterData[0] : json.results[0]
-//     setTrailer(trailer)
-//   };
-
-//   fetchMovieTrailer();
-// },[movieID])
-
 
 const useFetchAndDispatchMovieTrailer = ({movieID}) => {
 
@@ -34,6 +21,11 @@ const useFetchAndDispatchMovieTrailer = ({movieID}) => {
     useEffect(() => {
         getMovieTrailer();
     },[]);
+
+    // Return something if needed, such as the trailer data or a function to manually trigger the fetch
+    return {
+        fetchMovieTrailer: getMovieTrailer,
+    };
 };
 
 export default useFetchAndDispatchMovieTrailer;
