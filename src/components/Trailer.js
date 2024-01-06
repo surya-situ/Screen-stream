037e72ce19633f480c40d69f42b5e98df81ecf66
surya-situ <suryasocial55@gmail.com>
2024-01-06 10:03:14 +0530

    feat: video mute and unmute feature

--- src/components/Trailer.js
+++ src/components/Trailer.js
@@ -3,7 +3,9 @@ import useFetchAndDispatchMovieTrailer from '../hooks/useFetchAndDispatchTrailer
 
 const Trailer = ({movieID}) => {
 
-  const trailer = useSelector(store => store.movies.movieTrailer)
+  const trailer = useSelector(store => store.movies.movieTrailer);
+
+  const isMuted = useSelector((state) => state.sound.isMuted);
 
   useFetchAndDispatchMovieTrailer({movieID});
 
@@ -11,7 +13,7 @@ const Trailer = ({movieID}) => {
     <div>
       <iframe 
         className='w-screen h-fit aspect-video'
-        src={`https://www.youtube.com/embed/${trailer?.key}?version=3&autoplay=1&controls=0&showinfo=0&loop=1&mute=1&autohide=2&disablekb=1&rel=0&modestbranding=1&iv_load_policy=3`}
+        src={`https://www.youtube.com/embed/${trailer?.key}?version=3&autoplay=1&controls=0&showinfo=0&loop=1&mute=${isMuted ? 1 : 0}&autohide=2&disablekb=1&rel=0&modestbranding=1&iv_load_policy=3`}
         title="YouTube video player" 
         allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" 
         allowFullScreen
