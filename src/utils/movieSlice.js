f5d892d69de1e92fae8749caf9c9b2df3c8ef254
surya-situ <suryasocial55@gmail.com>
2024-01-08 20:32:23 +0530

    feat: trailer slice for trailer section

--- src/utils/movieSlice.js
+++ src/utils/movieSlice.js
@@ -3,14 +3,18 @@ import { createSlice } from "@reduxjs/toolkit";
 const movieSlice = createSlice({
     name: 'movies',
     initialState: {
-        allMovies: null
+        allMovies: null,
+        movieTrailer: null
     },
     reducers: {
         addAllMovies: (state, action) => {
             state.allMovies = action.payload
+        },
+        addMovieTrailer: (state, action) => {
+            state.movieTrailer = action.payload
         }
     }
 })
 
-export const {addAllMovies} = movieSlice.actions;
+export const {addAllMovies, addMovieTrailer} = movieSlice.actions;
 export default movieSlice.reducer
