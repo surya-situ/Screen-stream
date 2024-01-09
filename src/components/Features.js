1a6e6d47f1e01d6a80b08ae37c5f65a23dc22780
surya-situ <suryasocial55@gmail.com>
2024-01-09 19:56:30 +0530

    fix: resolve issue of scrolling with react-multi-carousel and made it responsive

--- src/components/Features.js
+++ src/components/Features.js
@@ -1,23 +1,51 @@
 import React from 'react';
 import { useSelector } from 'react-redux';
+import Carousel from 'react-multi-carousel';
+import 'react-multi-carousel/lib/styles.css';
 
 const Features = () => {
   const IMG_CDN = "https://image.tmdb.org/t/p/w500";
-
   const moviePoster = useSelector(store => store.movies?.allMovies ?? []);
-  console.log(moviePoster);
+
+  const responsive = {
+    desktop: {
+      breakpoint: { max: 3000, min: 1024 },
+      items: 7,
+      slidesToSlide: 7,
+    },
+    tablet: {
+      breakpoint: { max: 1024, min: 464 },
+      items: 5,
+      slidesToSlide: 5,
+    },
+    mobile: {
+      breakpoint: { max: 464, min: 0 },
+      items: 3,
+      slidesToSlide: 3,
+    },
+  };
 
   return (
-    <div className='flex overflow-x-auto no-scrollbar pl-[150px] my-8'>
-    <div className='flex'>
-      {
-        moviePoster.map((movie) => (
-          <img key={movie.id} src={`${IMG_CDN}${movie.poster_path}`} alt="poster" className='mr-6 cursor-pointer h-60'/>
-        ))
-      }
-      </div>
+    <div className='pl-[150px] my-8'>
+      <Carousel
+        responsive={responsive}
+        ssr={true} // Enable server-side rendering support
+        infinite={true}
+        draggable={true}
+        swipeable={true}
+        showDots={false}
+        removeArrowOnDeviceType={['tablet', 'mobile']}
+        transitionDuration={500}
+        containerClass="carousel-container"
+      >
+        {moviePoster.map((movie) => (
+          <div key={movie.id}>
+            <img src={`${IMG_CDN}${movie.poster_path}`} alt="poster" className='pr-8 cursor-pointer h-60'/>
+          </div>
+        ))}
+      </Carousel>
     </div>
-  )
+  );
 }
 
-export default Features
+export default Features;
