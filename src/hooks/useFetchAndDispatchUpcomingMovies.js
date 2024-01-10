eff6b6072be254672a1e4520c038b883eef37fb0
surya-situ <suryasocial55@gmail.com>
2024-01-10 14:20:16 +0530

    feat(hooks): custom hook for upcoming movies

---
+++ src/hooks/useFetchAndDispatchUpcomingMovies.js
@@ -0,0 +1,27 @@
+import { useEffect } from "react";
+import { useDispatch } from "react-redux";
+
+import { SCREEN_STREAM_API } from "../utils/constants";
+import { addUpcomingMovies } from "../utils/movieSlice";
+
+const useFetchAndDispatchUpcomingMovies = () => {
+
+    const dispatch = useDispatch();
+
+    const getUpcomingMovies  = async () => {
+
+        try {
+            const url = await fetch('https://api.themoviedb.org/3/movie/upcoming?language=en-US&page=1', SCREEN_STREAM_API)
+            const json = await url.json();
+            dispatch(addUpcomingMovies(json.results));
+        } catch (error) {
+            console.error("Error fetching popular movies:", error);
+        }
+    };
+
+    useEffect(() => {
+        getUpcomingMovies();
+    },[])
+};
+
+export default useFetchAndDispatchUpcomingMovies;
