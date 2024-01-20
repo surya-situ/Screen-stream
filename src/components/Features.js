02468193c803188bae0bfff36aa7597efcce1d9f
surya-situ <suryasocial55@gmail.com>
2024-01-20 15:11:23 +0530

    feat: movies, tv and people

--- src/components/Features.js
+++ src/components/Features.js
@@ -1,49 +1,28 @@
-import React from 'react';
 import { useSelector } from 'react-redux';
-import Carousel from 'react-multi-carousel';
-import 'react-multi-carousel/lib/styles.css';
+
+import Carousels from './template/Carousels';
 
 const Features = () => {
-  const IMG_CDN = "https://image.tmdb.org/t/p/w500";
-  const moviePoster = useSelector(store => store.movies?.allMovies ?? []);
-
-  const responsive = {
-    desktop: {
-      breakpoint: { max: 3000, min: 1024 },
-      items: 7,
-      slidesToSlide: 7,
-    },
-    tablet: {
-      breakpoint: { max: 1024, min: 464 },
-      items: 5,
-      slidesToSlide: 5,
-    },
-    mobile: {
-      breakpoint: { max: 464, min: 0 },
-      items: 3,
-      slidesToSlide: 3,
-    },
-  };
+  
+  const movies = useSelector(store => store.movies ?? []);
+  const tv = useSelector(store => store.tv ?? []);
+  const people = useSelector(store => store.people ?? []);
 
   return (
-    <div className='pl-[150px] my-8'>
-      <Carousel
-        responsive={responsive}
-        ssr={true} // Enable server-side rendering support
-        infinite={true}
-        draggable={true}
-        swipeable={true}
-        showDots={false}
-        removeArrowOnDeviceType={['tablet', 'mobile']}
-        transitionDuration={500}
-        containerClass="carousel-container"
-      >
-        {moviePoster.map((movie) => (
-          <div key={movie.id}>
-            <img src={`${IMG_CDN}${movie.poster_path}`} alt="poster" className='pr-8 cursor-pointer h-60'/>
-          </div>
-        ))}
-      </Carousel>
+    <div>
+
+      <Carousels title={"Now playing"} movies={movies.allMovies} />
+      <Carousels title={"Popular movies"} movies={movies.popularMovies} />
+      <Carousels title={"Top Rated "} movies={movies.topRatedMovies} />
+      <Carousels title={"Upcoming Movies"} movies={movies.upcomingMovies} />
+
+      <Carousels title={"Airing Today Tv"} movies={tv.airingToday} />
+      <Carousels title={"Top rated Tv"} movies={tv.topRatedTv} />
+      <Carousels title={"on the air Tv"} movies={tv.onTheAirTv} />
+      <Carousels title={"Popular Tv"} movies={tv.popularTv} />
+
+      <Carousels title={"Popular persons"} movies={people.popularPeople} />
+      
     </div>
   );
 }
