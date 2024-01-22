ff9b899e1fa7168b2d823b6cb0b5e4f3462f4f74
surya-situ <suryasocial55@gmail.com>
2024-01-22 19:28:36 +0530

    feat(hooks): renamed the file

---
+++ src/hooks/movies/useFetchAndDispatchPopularMovies.js
@@ -0,0 +1,26 @@
+import { useEffect } from "react";
+import { useDispatch } from "react-redux";
+
+import { SCREEN_STREAM_API } from "../../utils/constants";
+import { addPopularMovies } from "../../utils/movieSlice";
+
+const useFetchAndDispatchPopularMovies = () => {
+    const dispatch = useDispatch();
+
+    const getPopularMovies = async () => {
+        try {
+            const url = await fetch('https://api.themoviedb.org/3/movie/popular?language=en-US&page=1', SCREEN_STREAM_API)
+            const json = await url.json();
+            dispatch(addPopularMovies(json.results))
+        } catch (error) {
+            // Handle errors here
+            console.error("Error fetching popular movies:", error);
+        }
+    };
+
+    useEffect(() => {
+        getPopularMovies();
+    }, []);
+}
+
+export default useFetchAndDispatchPopularMovies;
