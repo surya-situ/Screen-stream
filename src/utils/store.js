eb086bb16f57e0630ffeb293180c700cca2af261
surya-situ <suryasocial55@gmail.com>
2024-01-22 19:39:18 +0530

    Added Two new reducer to the store

--- src/utils/store.js
+++ src/utils/store.js
@@ -1,11 +1,17 @@
 import { configureStore } from "@reduxjs/toolkit";
 
-import moviesReducer from './movieSlice'
+import moviesReducer from './movieSlice';
+import tvReducer from './tvSlice'
+import soundReducer from './soundSlice';
+import peopleReducer from './peopleSlice';
 
 const appStore = configureStore(
     {
         reducer: {
-            movies: moviesReducer
+            movies: moviesReducer,
+            tv: tvReducer,
+            people: peopleReducer,
+            sound: soundReducer,
         }
     }
 )
