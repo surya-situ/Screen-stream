45a4c86d6e142319cc735edc7101e31744542c11
surya-situ <suryasocial55@gmail.com>
2024-01-28 15:37:27 +0530

    fix(bug): implimented the dependency array to prevent infinite call

--- src/hooks/movies/useFetchAndDispatchTopRatedMovies.js
+++ src/hooks/movies/useFetchAndDispatchTopRatedMovies.js
@@ -21,7 +21,7 @@ const useFetchAndDispatchTopRatedMovies = () => {
 
     useEffect(() => {
         getTopRatedMovies();
-    });
+    },[]);
 };
 
 export default useFetchAndDispatchTopRatedMovies;
