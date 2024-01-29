3ec14f02ba905bbf04637f4f3e223edd0bda4ce4
surya-situ <suryasocial55@gmail.com>
2024-01-29 11:59:01 +0530

    feat: Implimented new routing to watch page

--- src/index.js
+++ src/index.js
@@ -14,6 +14,7 @@ import SearchPage from './pages/SearchPage';
 import TvPage from './pages/TvPage';
 import MoviePage from './pages/MoviePage';
 import CategoryPage from './pages/CategoryPage';
+import WatchPage from './pages/WatchPage';
 
 
 const appRouter = createBrowserRouter([
@@ -53,7 +54,10 @@ const appRouter = createBrowserRouter([
         path: '/loginPage',
         element: <LoginPage />
       },
-      
+      {
+        path: '/watch/:id',
+        element: <WatchPage />
+      }
     ]
   }
 ])
