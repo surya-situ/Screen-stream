81cabeaf83e0b9bf4b18c6c6f4d7d6fcc2ddcf08
surya-situ <suryasocial55@gmail.com>
2024-02-02 19:43:02 +0530

    feat(style): Rendered the free or premium tag to each posters

--- src/components/template/Posters.js
+++ src/components/template/Posters.js
@@ -2,7 +2,11 @@ import React from 'react';
 
 import { IMG_CDN } from '../../utils/constants';
 
-const Posters = ({posterPath}) => {
+const Posters = ({posterPath, isFreeCategory}) => {
+
+  const movieType = isFreeCategory ? 'Free' : 'Premium';
+  const labelClassName = isFreeCategory ? 'bg-blue-600' : 'bg-red-500';
+  
   return (
     <div>
         <img 
@@ -10,6 +14,10 @@ const Posters = ({posterPath}) => {
             alt="poster" 
             className='transition-transform duration-300 ease-out rounded-lg cursor-pointer w-44 h-60 hover:scale-110'
         />
+
+      <p className={`absolute px-2 py-1 text-sm font-semibold text-white rounded-md left-1 top-1 tracking-wider ${labelClassName}`}>
+        {movieType}
+      </p>
     </div>
   )
 }
