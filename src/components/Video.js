d3537995a015cd6c1e0f9541a9d70fe3b8fcf1af
surya-situ <suryasocial55@gmail.com>
2024-02-02 19:01:22 +0530

    fix(style): changed the screen to width for full view and remove the margin top

--- src/components/Video.js
+++ src/components/Video.js
@@ -8,9 +8,9 @@ const Video = ({movieID}) => {
     useFetchAndDispatchVideo({movieID})
 
   return (
-    <div className='mt-40'>
+    <div>
       <iframe 
-        className='w-[1250px] h-fit aspect-video'
+        className='w-screen h-fit aspect-video'
         src={`https://www.youtube.com/embed/${movieVideo?.key}?version=3&showinfo=1&loop=1&mute=1`}
         title="YouTube video player" 
         allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" 
