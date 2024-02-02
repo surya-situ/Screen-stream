abc1bcc8eb75764f1a6fcd92abb87d74e1e26f53
surya-situ <suryasocial55@gmail.com>
2024-02-02 16:56:41 +0530

    feat(hook): custom to fetch and dispatch recommended movies

---
+++ src/hooks/movies/useFetchAndDispatchRecommendedMovies.js
@@ -0,0 +1,28 @@
+import { useDispatch } from "react-redux";
+import { SCREEN_STREAM_API } from "../../utils/constants";
+
+
+import { addRecommendMovies } from "../../utils/movieSlice";
+import { useEffect } from "react";
+
+const useFetchAndDispatchRecommendedMovies = ({idNumber}) => {
+
+    const dispatch = useDispatch()
+  
+    const getRecommendedMovies = async () => {
+
+        try {
+            const fetchData = await fetch(`https://api.themoviedb.org/3/movie/${idNumber}/recommendations?language=en-US&page=1`, SCREEN_STREAM_API)
+            const jsonData = await fetchData.json()
+            dispatch(addRecommendMovies(jsonData.results))
+        } catch (error) {
+            console.log('Error in fetching', error);
+        }
+    };
+
+    useEffect(() => {
+        getRecommendedMovies()
+    },[])
+}
+
+export default useFetchAndDispatchRecommendedMovies;
