84fd86cddca595ffe41c412128b2dc7b419f50ee
surya-situ <suryasocial55@gmail.com>
2024-02-06 13:28:43 +0530

    feat,style: Added new section for details and changed the hover size

--- src/pages/SearchPage.js
+++ src/pages/SearchPage.js
@@ -42,14 +42,18 @@ const SearchPage = () => {
       <div className="flex flex-wrap justify-center mt-8">
         {
           searchItems.filter((movie) => movie.backdrop_path).map((Movie) => (
-            <div key={Movie.id} className='flex-shrink-0 mx-6 my-6'>
+            <div key={Movie.id} className='relative mx-6 my-6 hover:scale-125'>
               <Link to={`/watch/${Movie.id}`}>
               <img 
-                className='w-[360px] rounded-lg overflow-hidden cursor-pointer hover:scale-105 ' 
+                className='w-[360px] rounded-lg overflow-hidden cursor-pointer' 
                 src={IMG_CDN + Movie.backdrop_path} 
                 alt={Movie.original_name || Movie.profile_path}
                 style={{ margin: '-1px' }}
               />
+              <div className="absolute bottom-0 left-0 p-3 text-white transition-opacity duration-300 bg-black bg-opacity-75 rounded-bl-lg rounded-br-lg opacity-0 hover:opacity-100">
+                <h3 className="text-lg font-semibold">{Movie.original_title || Movie.original_name}</h3>
+                <p className="text-[12px] overflow-hidden" style={{ display: '-webkit-box', WebkitLineClamp: 4, WebkitBoxOrient: 'vertical' }}>{Movie.overview}</p>
+              </div>
               </Link>
             </div>
           ))
