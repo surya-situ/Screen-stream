40669d3f0f31fb0caa3500115a76b30a9be6e8f0
surya-situ <suryasocial55@gmail.com>
2024-02-06 12:51:48 +0530

    Remove CategoryPage.js

--- src/pages/CategoryPage.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const CategoryPage = () => {
-  return (
-    <div className='flex items-center justify-center h-screen'>CategoryPage</div>
-  )
-}
-
-export default CategoryPage
