549a43a5c0e748f3eeab830a10085c3e26f44444
surya-situ <suryasocial55@gmail.com>
2024-02-06 12:20:57 +0530

    feat(hooks): Custom hook for fetch and dispatch search items

---
+++ src/hooks/search/useFetchAndDispatchSearch.js
@@ -0,0 +1,54 @@
+import { useEffect } from 'react'
+import { useDispatch } from 'react-redux'
+
+import { SCREEN_STREAM_API } from '../../utils/constants';
+import { addSearch } from '../../utils/searchSlice';
+
+// const useFetchAndDispatchSearch = ({searchInput}) => {
+  
+//     const dispatch = useDispatch();
+
+//     const getSearchResult = async () => {
+
+//         try {
+//             const fetchData = await fetch(`https://api.themoviedb.org/3/search/multi?query=${searchInput}&include_adult=false&language=en-US&page=1`, SCREEN_STREAM_API)
+//             const jsonData = await fetchData.json();
+//             dispatch(addSearch(jsonData.results))
+//         } catch (error) {
+//             console.log('Error in fetching', error);
+//         }
+//     }
+
+//     useEffect(() => {
+//         getSearchResult()
+//     },[])
+// }
+
+// export default useFetchAndDispatchSearch;
+
+const useFetchAndDispatchSearch = (searchQuery) => {
+    const dispatch = useDispatch();
+  
+    useEffect(() => {
+      const getSearchResult = async () => {
+        try {
+          const fetchData = await fetch(
+            `https://api.themoviedb.org/3/search/multi?query=${searchQuery}&include_adult=false&language=en-US&page=1`,
+            SCREEN_STREAM_API
+          );
+          const jsonData = await fetchData.json();
+          dispatch(addSearch(jsonData.results));
+        } catch (error) {
+          console.log('Error in fetching', error);
+        }
+      };
+  
+      if (searchQuery.trim() !== '') {
+        getSearchResult();
+      }
+    }, [searchQuery, dispatch]);
+  
+    // No need to return anything from a custom hook
+  };
+  
+  export default useFetchAndDispatchSearch;
