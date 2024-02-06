7c8a8d9c9bb95484b098b45dab33fed869506800
surya-situ <suryasocial55@gmail.com>
2024-02-06 12:29:33 +0530

    feat,style: Changed the overall look of banner and added watch trailer button to the currosponded movie or tv show

--- src/components/template/Banner.js
+++ src/components/template/Banner.js
@@ -1,44 +1,40 @@
 import React from 'react';
 
 import { IMG_CDN } from '../../utils/constants';
+import { Link } from 'react-router-dom';
 
 const Banner = ({banner}) => {
 
     return (
       <div className='pl-[150px] pr-[50px] mt-10'>
-        <div className='flex flex-row bg-gray-950 rounded-xl'>
+        <div className='flex flex-row bg-gray-900 rounded-xl'>
           <img 
               src={`${IMG_CDN}${banner.backdrop_path}`} 
               alt="banner"
-              className='rounded-s-xl h-60' 
+              className='h-full rounded-s-xl' 
           />
 
-          <div className='flex flex-col my-10 ml-20'>
+          <div className='relative flex flex-col w-full my-12 ml-20'>
 
-            <div className='flex flex-row items-center justify-center mb-4'>
+            <div className='flex flex-row items-center mb-4'>
 
               <div className='flex flex-col items-center justify-center text-sm font-semibold leading-3'>
                 <span>stream</span>
-                <span className='text-green-500'>special</span>
+                <span className='text-blue-500'>special</span>
               </div>
 
               <h1 className='ml-4 text-4xl font-semibold'>{banner.title || banner.original_name}</h1>
             </div>
 
-            <p className='flex items-center text-sm font-medium'>
-              <span className='font-normal opacity-90'>
-                {
-                  banner.release_date ? "Release data" : "First aired"
-                }
-              </span>
-              <span className='ml-2 text-xl text-green-500'>{banner.release_date || banner.first_air_date } </span>
+            <p className='tracking-wider font-semi-bold'>
+              Watch the {banner.title || banner.original_name} on screen-stream before it premieres anywhere else.
             </p>
 
-            <p className='flex items-center text-sm font-medium'>
-              <span className='font-normal opacity-90'>Voting average </span>
-              <span className='ml-2 text-xl text-green-500 mr-[3px]'>{Math.round(banner.vote_average) }</span> 
-              <span className='text-xl'>/ 10</span>
-            </p>
+            <Link to={`/watch/${banner.id}`}>
+              <button className='absolute bottom-0 px-12 py-3 text-xl font-semibold tracking-wider transition duration-300 ease-in-out transform bg-gray-600 rounded-lg opacity-90 hover:scale-105 '>
+                Watch trailer
+              </button>
+            </Link>
           </div>
 
         </div>
