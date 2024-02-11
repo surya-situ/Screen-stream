374d957d5efa5580e61e9d3adf32383bbfe23f79
surya-situ <suryasocial55@gmail.com>
2024-02-11 23:31:35 +0530

    feat(style): Hide header on mobile view

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
 import { Outlet } from "react-router-dom";
 import { Provider } from "react-redux";
 
@@ -9,10 +9,28 @@ import Footer from "./components/Footer";
 
 const App = () => {
 
+  const [isMobile, setIsMobile] = useState(false);
+
+  useEffect(() => {
+    // Function to check if the screen width is less than or equal to 600px
+    const handleResize = () => {
+      setIsMobile(window.innerWidth <= 600);
+    };
+
+    // Add event listener for window resize
+    window.addEventListener("resize", handleResize);
+
+    // Call handleResize initially to set initial state
+    handleResize();
+
+    // Clean up event listener on component unmount
+    return () => window.removeEventListener("resize", handleResize);
+  }, []);
+
   return (
     <Provider store={appStore}>
       <div className="p-0 m-0 text-white bg-gradient-to-b from-slate-950 via-slate-950 to-black">
-        <Header />
+      {!isMobile && <Header />}
         <Outlet />
         <Footer />
       </div>
