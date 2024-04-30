30e51a67404b0d6551800cc2a8c60f80f03b39b7
surya-situ <suryasocial55@gmail.com>
2024-04-30 16:05:21 +0530

    fix: Remove the faulty responsive design

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,5 +1,3 @@
-import { useState, useEffect } from 'react';
-
 import Hero from '../components/hero'
 import Features from '../components/Features'
 
@@ -39,28 +37,10 @@ const HomePage = () => {
   // POPULAR FIGURES
   useFetchAndDispatchPopularPeople();
 
-  const [isMobile, setIsMobile] = useState(false);
-
-    useEffect(() => {
-      // Function to check if the screen width is less than or equal to 600px
-      const handleResize = () => {
-        setIsMobile(window.innerWidth <= 600);
-      };
-  
-      // Add event listener for window resize
-      window.addEventListener("resize", handleResize);
-  
-      // Call handleResize initially to set initial state
-      handleResize();
-  
-      // Clean up event listener on component unmount
-      return () => window.removeEventListener("resize", handleResize);
-    }, []);
-
 
   return (
     <div >
-      {!isMobile && <Hero />}
+      <Hero />
       <Features />
     </div>
   )
