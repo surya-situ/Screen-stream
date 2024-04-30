494f142759aca42233b607a488de2ce2e36345b8
surya-situ <suryasocial55@gmail.com>
2024-04-30 16:02:53 +0530

    fix: Removed the faulty responsive design as the ui style is not that mobile friendly

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-import React, {useState, useEffect} from "react";
 import { Outlet } from "react-router-dom";
 import { Provider } from "react-redux";
 
@@ -9,28 +8,10 @@ import Footer from "./components/Footer";
 
 const App = () => {
 
-  const [isMobile, setIsMobile] = useState(false);
-
-  useEffect(() => {
-    // Function to check if the screen width is less than or equal to 600px
-    const handleResize = () => {
-      setIsMobile(window.innerWidth <= 600);
-    };
-
-    // Add event listener for window resize
-    window.addEventListener("resize", handleResize);
-
-    // Call handleResize initially to set initial state
-    handleResize();
-
-    // Clean up event listener on component unmount
-    return () => window.removeEventListener("resize", handleResize);
-  }, []);
-
   return (
     <Provider store={appStore}>
       <div className="p-0 m-0 text-white bg-gradient-to-b from-slate-950 via-slate-950 to-black">
-      {!isMobile && <Header />}
+        <Header />
         <Outlet />
         <Footer />
       </div>
