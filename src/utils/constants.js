5802328ce8025532cda83da741910a403ec003fd
surya-situ <suryasocial55@gmail.com>
2024-11-20 14:12:08 +0530

    fix: fixed minor error of api key

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,9 +1,5 @@
 export const SCREEN_STREAM_API = {
-    method: 'GET',
-    headers: {
-      accept: 'application/json',
-      Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIwYWY4ZTlmYzUzMmIwMTk0ZWI3N2E3MTU0MTNiYTgxZCIsInN1YiI6IjY1N2U4M2VmZjFiNTcxMDhiMWFkOWExMSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.RCc5CFuo3d1zw2m7Q3qfihr86EdYOsoP5gqFLoCyLuA'
-    }
+  use_your_TMDB_api
 };
 
 export const IMG_CDN = "https://image.tmdb.org/t/p/w500";
