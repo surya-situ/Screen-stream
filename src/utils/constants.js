fe9d46e03be882ea2c6c55eed7a95363eaa1ed56
surya-situ <suryasocial55@gmail.com>
2024-11-20 14:26:33 +0530

    fix: fixed the api config error

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,5 +1,13 @@
+import dotenv from "dotenv";
+
+dotenv.config();
+
 export const SCREEN_STREAM_API = {
-  use_your_TMDB_api
+  method: 'GET',
+  headers: {
+    accept: 'application/json',
+    Authorization: `Bearer ${process.env.SCREEN_STREAM_API}`
+  }
 };
 
 export const IMG_CDN = "https://image.tmdb.org/t/p/w500";
