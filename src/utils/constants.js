83d5b1498635efc6d9b579840c593376edd50ba2
surya-situ <suryasocial55@gmail.com>
2025-01-09 14:42:47 +0530

    change the variable

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,12 +1,8 @@
-import dotenv from "dotenv";
-
-dotenv.config();
-
 export const SCREEN_STREAM_API = {
   method: 'GET',
   headers: {
     accept: 'application/json',
-    Authorization: `Bearer ${process.env.SCREEN_STREAM_API}`
+    Authorization: `Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIwYWY4ZTlmYzUzMmIwMTk0ZWI3N2E3MTU0MTNiYTgxZCIsInN1YiI6IjY1N2U4M2VmZjFiNTcxMDhiMWFkOWExMSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.RCc5CFuo3d1zw2m7Q3qfihr86EdYOsoP5gqFLoCyLuA`
   }
 };
 
